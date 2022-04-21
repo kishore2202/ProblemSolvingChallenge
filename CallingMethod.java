6b5773c5d6038ce0dd1c068ecac136dbb9a14295
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-04-21 07:38:48 +0530

    Add files via upload

---
+++ CallingMethod.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class CallingMethod {
+public static void method(int a,int b)
+{
+	int c=a+b;
+	System.out.println(c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CallingMethod obj=new CallingMethod();
+		obj.method(2, 6);
+	}
+
+}
