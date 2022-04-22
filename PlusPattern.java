61a13ce6c557bfe58abc65de25742ef5db7abd94
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-04-22 07:29:53 +0530

    Add files via upload

---
+++ PlusPattern.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class PlusPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=11;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==n/2||j==n/2)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
