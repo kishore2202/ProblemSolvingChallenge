fb96a0ef9641429293b0ae07c826011b0a92cbf1
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-04-26 17:54:23 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class LinearSearch {
+
+	public static int linear(int[] arr,int element)
+	{
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==element)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {4,1,6,3,8,2};
+		System.out.println(linear(arr,6));
+	}
+
+}
