9dda6b14219f8def7d3a35d4d7add8336b9a503c
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-04-30 07:40:25 +0530

    Add files via upload

---
+++ EncapsulationDemo.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class EncapsulationDemo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student obj=new Student("abc",15);
+		obj.setName("xyz");
+		obj.getName();
+	}
+
+}
+
+class Student extends EncapsulationDemo{
+	private String name;
+	private int roll;
+	Student(String name,int roll)
+	{
+		this.name=name;
+		this.roll=roll;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+}
