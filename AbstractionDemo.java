1d73797cf55c40592d9bc9d43e33021a64638df7
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-05-01 17:38:27 +0530

    Add files via upload

---
+++ AbstractionDemo.java
@@ -0,0 +1,19 @@
+package practice;
+
+public abstract class AbstractionDemo {
+
+	public void call() {
+		System.out.println("Calling....");
+	}
+	public void record() {
+		System.out.println("Recording.....");
+	}
+	
+	abstract public void msg();
+	
+	abstract class Abc extends AbstractionDemo{
+	
+	abstract public void msg();
+	
+}
+}
