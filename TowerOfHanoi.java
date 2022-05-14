a9a98ae3758002a569f8c7ad36f62c9724547d63
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-05-14 14:49:02 +0530

    Add files via upload

---
+++ TowerOfHanoi.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class TowerOfHanoi {
+public static void Toh(int n,char a,char b,char c)
+{
+	if(n==1)
+	{
+		System.out.println("Move 1 from "+a+" to "+c);
+		return;
+	}
+	Toh(n-1,a,c,b);
+	System.out.println("Move "+n+" from "+a+" to "+c);
+	Toh(n-1,b,a,c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Toh(3,'a','b','c');
+	}
+
+}
