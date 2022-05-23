28728bb1230929e148f1789ae55d0dcc2a846aed
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-05-23 20:37:24 +0530

    Add files via upload

---
+++ FindingUnitDigit.java
@@ -0,0 +1,13 @@
+package github;
+
+public class FindingUnitDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5123;
+		int b=(a%10);
+		int c=(a%100)/10;
+		System.out.println(b+" "+c);
+	}
+
+}
