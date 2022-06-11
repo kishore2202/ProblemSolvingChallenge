b6cab7230d77afa234950ad057658a4dd10c67a0
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-06-11 19:02:29 +0530

    Create DivideArrayIntoEqualPairs.java

---
+++ leetcode/DivideArrayIntoEqualPairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean divideArray(int[] in) {
+       boolean bool[]=new boolean[in.length];
+	   int count=0;
+	   for(int i=0;i<in.length;i++) {
+		   for(int j=i+1;j<in.length;j++) {
+			   if(in[i]==in[j]&& bool[i]!=true&& bool[j]!=true) {
+				   count++;
+				   bool[i]=true; 
+				   bool[j]=true;
+			   }
+		   }
+	   }
+	   return count==(in.length/2);
+    }
+}
