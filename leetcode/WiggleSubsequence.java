dc494999a11da7eaf79d52d9e48928ef4f153bb8
Kishore R <92428116+kishore2202@users.noreply.github.com>
2022-07-03 17:59:58 +0530

    Update WiggleSubsequence.java

--- leetcode/WiggleSubsequence.java
+++ leetcode/WiggleSubsequence.java
@@ -2,21 +2,21 @@ class Solution {
     public int wiggleMaxLength(int[] nums) {
          if (nums.length < 2)
             return nums.length;
-        int[] up = new int[nums.length];
-        int[] down = new int[nums.length];
-        up[0] = down[0] = 1;
+        int[] u = new int[nums.length];
+        int[] d = new int[nums.length];
+        u[0] = d[0] = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] > nums[i - 1]) {
-                up[i] = down[i - 1] + 1;
-                down[i] = down[i - 1];
+                u[i] = d[i - 1] + 1;
+                d[i] = d[i - 1];
             } else if (nums[i] < nums[i - 1]) {
-                down[i] = up[i - 1] + 1;
-                up[i] = up[i - 1];
+                d[i] = u[i - 1] + 1;
+                u[i] = u[i - 1];
             } else {
-                down[i] = down[i - 1];
-                up[i] = up[i - 1];
+                d[i] = d[i - 1];
+                u[i] = u[i - 1];
             }
         }
-        return Math.max(down[nums.length - 1], up[nums.length - 1]);
+        return Math.max(d[nums.length - 1], u[nums.length - 1]);
     }
 }
