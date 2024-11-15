7cb90083984ec049a859a0e05e95c5d548050a04
Kishore R <92428116+kishore2202@users.noreply.github.com>
2024-11-15 19:31:10 +0530

    Create ElementsRemoval.java

---
+++ ElementsRemoval.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ElementsRemoval {
+
+	public static int solve(int[] A)
+	{
+		Arrays.sort(A);
+		int sum = 0;
+        for(int i:A)
+        {
+            sum+=i;
+        }
+        int total=sum;
+        for(int i=A.length-2;i>=0;i--)
+        {
+            sum=sum-A[i+1];
+            total+=sum;
+        }
+		return total;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {5,4,3,2,1};
+		System.out.println(solve(arr));
+	}
+
+}
